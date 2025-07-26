26392f286cd1714cf448f0d5fd6414e767a0f580
huhuhu88844 <bisukane@gmail.com>
2025-07-27 03:21:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const pick = require("./api/pick");
+const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use("/api/pick", pick);
+// Cấu hình phục vụ file tĩnh từ thư mục public
+app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
